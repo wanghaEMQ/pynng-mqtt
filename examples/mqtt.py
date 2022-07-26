92be0e86b084197f0d440edcb9994e2fd001874a
wanghaemq <wangwei@emqx.io>
2022-07-26 10:47:45 +0800

    Update the mqtt example.

--- examples/mqtt.py
+++ examples/mqtt.py
@@ -5,45 +5,25 @@ Like Pipeline, it also can perform load-balancing.
 This is the only reliable messaging pattern in the suite, as it automatically will retry if a request is not matched with a response.
 
 """
-import datetime
 import pynng
 import curio
 
-DATE = "DATE"
-
-address = "ipc:///tmp/reqrep.ipc"
-
-
-async def node0(sock):
-    async def response_eternally():
-        while True:
-            msg = await sock.arecv_msg()
-            content = msg.bytes.decode()
-            if DATE == content:
-                print("NODE0: RECEIVED DATE REQUEST")
-                date = str(datetime.datetime.now())
-                await sock.asend(date.encode())
-
-    sock.listen(address)
-    return await curio.spawn(response_eternally)
-
-
-async def node1():
-    with pynng.Req0() as sock:
-        sock.dial(address)
-        print(f"NODE1: SENDING DATE REQUEST")
-        await sock.asend(DATE.encode())
-        msg = await sock.arecv_msg()
-        print(f"NODE1: RECEIVED DATE {msg.bytes.decode()}")
-
+address = "mqtt-tcp://127.0.0.1:1883"
 
 async def main():
-    with pynng.Rep0() as rep:
-        n0 = await node0(rep)
-        await curio.sleep(1)
+    with pynng.Mqtt() as mqtt:
+        print(f"Make a connect msg")
+        mqttmsg = pynng.Mqttmsg()
+        mqttmsg.set_packet_type(1) # 0x01 Connect
+        mqttmsg.set_connect_proto_version(4) # MqttV311
+        print(f"Dialer start.")
+        mqtt.dial_msg(address, mqttmsg)
+        print(f"dialer done.")
+        """
         await node1()
 
         await n0.cancel()
+        """
 
 
 if __name__ == "__main__":
@@ -51,4 +31,4 @@ if __name__ == "__main__":
         curio.run(main)
     except KeyboardInterrupt:
         # that's the way the program *should* end
-        pass
+        exit(0)
